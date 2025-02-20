89745001d555ef45b87225225477c97c32789a44
Roman De Rose <romandr37@gmail.com>
2025-02-20 01:45:08 -0300

    Mejorando diseño y agregado de código CSS a la box

--- script.js
+++ script.js
@@ -28,20 +28,31 @@ const shadowColor = document.getElementById("color-shadow");
 const shadowOpacity = document.getElementById("opacity-shadow");
 
 function hex2rgb(hex) {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
+  const red = parseInt(hex.slice(1, 3), 16);
+  const green = parseInt(hex.slice(3, 5), 16);
+  const blue = parseInt(hex.slice(5, 7), 16);
 
-  return { r, g, b };
+  return { red, green, blue };
+}
+
+function textColor(color, opacity) {
+  const colorHex = hex2rgb(color);
+  const { red, green, blue } = colorHex;
+  const colorFinal = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+
+  return colorFinal;
 }
 
 function actualizeShadow(el, x, y, blur, spread, color, opacity) {
-  const colorRed = hex2rgb(color).r;
-  const colorGreen = hex2rgb(color).g;
-  const colorBlue = hex2rgb(color).b;
-  const colorFinal = `rgba(${colorRed}, ${colorGreen}, ${colorBlue}, ${opacity})`;
+  const colorShadow = textColor(color, opacity);
 
-  return (el.style.boxShadow = `${x}px ${y}px ${blur}px ${spread}px ${colorFinal}`);
+  return (el.style.boxShadow = `${x}px ${y}px ${blur}px ${spread}px ${colorShadow}`);
+}
+
+function actualizeCodeShadow(x, y, blur, color, opacity) {
+  const colorShadow = textColor(color, opacity);
+
+  return `box-shadow: ${x}px ${y}px ${blur}px ${colorShadow};`;
 }
 
 let x = shadowX.value;
@@ -54,39 +65,85 @@ let opacity = shadowOpacity.value;
 // SETEO BOX-SHADOW
 actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
 
+// SETEO CODIGO CSS BOX-SHADOW
+const codeShadow = document.getElementById("code-box-shadow");
+codeShadow.textContent = actualizeCodeShadow(x, y, blurShadow, color, opacity);
+
 // MODIFICACION DE BOX-SHADOW
 shadowX.addEventListener("input", (e) => {
   e.preventDefault();
   x = e.target.value;
   actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
+  codeShadow.textContent = actualizeCodeShadow(
+    x,
+    y,
+    blurShadow,
+    color,
+    opacity
+  );
 });
 
 shadowY.addEventListener("input", (e) => {
   e.preventDefault();
   y = e.target.value;
   actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
+  codeShadow.textContent = actualizeCodeShadow(
+    x,
+    y,
+    blurShadow,
+    color,
+    opacity
+  );
 });
 
 shadowBlur.addEventListener("input", (e) => {
   e.preventDefault();
   blurShadow = e.target.value;
   actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
+  codeShadow.textContent = actualizeCodeShadow(
+    x,
+    y,
+    blurShadow,
+    color,
+    opacity
+  );
 });
 
 shadowSpread.addEventListener("input", (e) => {
   e.preventDefault();
   spread = e.target.value;
   actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
+  codeShadow.textContent = actualizeCodeShadow(
+    x,
+    y,
+    blurShadow,
+    color,
+    opacity
+  );
 });
 
 shadowColor.addEventListener("input", (e) => {
   e.preventDefault();
   color = e.target.value;
   actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
+  codeShadow.textContent = actualizeCodeShadow(
+    x,
+    y,
+    blurShadow,
+    color,
+    opacity
+  );
 });
 
 shadowOpacity.addEventListener("input", (e) => {
   e.preventDefault();
   opacity = e.target.value;
   actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
+  codeShadow.textContent = actualizeCodeShadow(
+    x,
+    y,
+    blurShadow,
+    color,
+    opacity
+  );
 });
