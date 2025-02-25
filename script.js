6f5f974634c10556ea6935dc5a5ee9ab73d0e1a7
Roman De Rose <romandr37@gmail.com>
2025-02-24 23:26:45 -0300

    Rediseño y lógica copy código CSS

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ const boxReceivesShadow = document.getElementById("box-shadow");
 const inputBoxContainer = document.getElementById("background-box");
 const inputBoxReceivesShadow = document.getElementById("background-shadow-box");
 
-// CODIGO CSS BOX SHADOW
-//  despues
-
 // BACKGROUND boxContainer
-inputBoxContainer.addEventListener("input", (e) => {
-  e.preventDefault();
-  boxContainer.style.background = e.target.value;
-});
+inputBoxContainer.addEventListener(
+  "input",
+  (e) => (boxContainer.style.background = e.target.value)
+);
 
 // BACKGROUND boxReceivesShadow
-inputBoxReceivesShadow.addEventListener("input", (e) => {
-  e.preventDefault();
-  boxReceivesShadow.style.background = e.target.value;
-});
+inputBoxReceivesShadow.addEventListener(
+  "input",
+  (e) => (boxReceivesShadow.style.background = e.target.value)
+);
 
 // INPUTS DE LAS SOMBRAS
 const shadowX = document.getElementById("x-shadow");
@@ -26,6 +23,12 @@ const shadowBlur = document.getElementById("blur-shadow");
 const shadowSpread = document.getElementById("spread-shadow");
 const shadowColor = document.getElementById("color-shadow");
 const shadowOpacity = document.getElementById("opacity-shadow");
+const shadowInputs = document.querySelectorAll(".input-range");
+
+// CODIGO CSS BOX SHADOW
+const codeShadow = document.getElementById("code-box-shadow");
+
+// VALORES PROPIEDAS DE BOX SHADOW
 
 function hex2rgb(hex) {
   const red = parseInt(hex.slice(1, 3), 16);
@@ -49,12 +52,13 @@ function actualizeShadow(el, x, y, blur, spread, color, opacity) {
   return (el.style.boxShadow = `${x}px ${y}px ${blur}px ${spread}px ${colorShadow}`);
 }
 
-function actualizeCodeShadow(x, y, blur, color, opacity) {
+function actualizeCodeShadow(x, y, blur, spread, color, opacity) {
   const colorShadow = textColor(color, opacity);
 
-  return `box-shadow: ${x}px ${y}px ${blur}px ${colorShadow};`;
+  return `box-shadow: ${x}px ${y}px ${blur}px ${spread}px ${colorShadow};`;
 }
 
+// SETEO BOX-SHADOW
 let x = shadowX.value;
 let y = shadowY.value;
 let blurShadow = shadowBlur.value;
@@ -62,88 +66,68 @@ let spread = shadowSpread.value;
 let color = shadowColor.value;
 let opacity = shadowOpacity.value;
 
-// SETEO BOX-SHADOW
 actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
 
+// SETEO VALORES CSS BOX-SHADOW
+
 // SETEO CODIGO CSS BOX-SHADOW
-const codeShadow = document.getElementById("code-box-shadow");
-codeShadow.textContent = actualizeCodeShadow(x, y, blurShadow, color, opacity);
+codeShadow.textContent = actualizeCodeShadow(
+  x,
+  y,
+  blurShadow,
+  spread,
+  color,
+  opacity
+);
 
 // MODIFICACION DE BOX-SHADOW
-shadowX.addEventListener("input", (e) => {
-  e.preventDefault();
-  x = e.target.value;
-  actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
-  codeShadow.textContent = actualizeCodeShadow(
-    x,
-    y,
-    blurShadow,
-    color,
-    opacity
-  );
+shadowInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    const textValue = input.previousElementSibling.children[1];
+    x = shadowX.value;
+    y = shadowY.value;
+    blurShadow = shadowBlur.value;
+    spread = shadowSpread.value;
+    color = shadowColor.value;
+    opacity = shadowOpacity.value;
+
+    input.getAttribute("type") === "range"
+      ? (textValue.textContent = e.target.value + "px")
+      : null;
+
+    actualizeShadow(
+      boxReceivesShadow,
+      x,
+      y,
+      blurShadow,
+      spread,
+      color,
+      opacity
+    );
+
+    codeShadow.textContent = actualizeCodeShadow(
+      x,
+      y,
+      blurShadow,
+      spread,
+      color,
+      opacity
+    );
+  });
 });
 
-shadowY.addEventListener("input", (e) => {
-  e.preventDefault();
-  y = e.target.value;
-  actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
-  codeShadow.textContent = actualizeCodeShadow(
-    x,
-    y,
-    blurShadow,
-    color,
-    opacity
-  );
-});
-
-shadowBlur.addEventListener("input", (e) => {
-  e.preventDefault();
-  blurShadow = e.target.value;
-  actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
-  codeShadow.textContent = actualizeCodeShadow(
-    x,
-    y,
-    blurShadow,
-    color,
-    opacity
-  );
-});
-
-shadowSpread.addEventListener("input", (e) => {
-  e.preventDefault();
-  spread = e.target.value;
-  actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
-  codeShadow.textContent = actualizeCodeShadow(
-    x,
-    y,
-    blurShadow,
-    color,
-    opacity
-  );
-});
-
-shadowColor.addEventListener("input", (e) => {
-  e.preventDefault();
-  color = e.target.value;
-  actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
-  codeShadow.textContent = actualizeCodeShadow(
-    x,
-    y,
-    blurShadow,
-    color,
-    opacity
-  );
-});
-
-shadowOpacity.addEventListener("input", (e) => {
-  e.preventDefault();
-  opacity = e.target.value;
-  actualizeShadow(boxReceivesShadow, x, y, blurShadow, spread, color, opacity);
-  codeShadow.textContent = actualizeCodeShadow(
-    x,
-    y,
-    blurShadow,
-    color,
-    opacity
+// COPIAR CODIGO CSS BOX-SHADOW (elemento codeShadow)
+const alertCopy = document.querySelector(".alert-copy");
+codeShadow.addEventListener("click", (e) => {
+  navigator.clipboard.writeText(e.target.textContent).then(
+    () => {
+      alertCopy.style.transform = "translateY(0)";
+      setTimeout(() => {
+        alertCopy.style.transform = "translateY(-100px)";
+      }, 2000);
+    },
+    (err) => {
+      console.error("No se pudo copiar el texto: ", err);
+    }
   );
 });
